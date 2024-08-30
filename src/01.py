0a4151ba342d0bac3981511bb366028da8aa7e22
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2024-08-30 18:03:37 -0300

    🎉 Inicial Commit

---
+++ src/01.py
@@ -0,0 +1,4 @@
+# 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+# Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+# Imprimir(SOMA);
+# Ao final do processamento, qual será o valor da variável SOMA?
---
+++ src/05.py
@@ -0,0 +1,5 @@
+# 5) Escreva um programa que inverta os caracteres de um string.
+
+# IMPORTANTE:
+# a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+# b) Evite usar funções prontas, como, por exemplo, reverse;
