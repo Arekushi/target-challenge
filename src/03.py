a27045024ef76541f074d1effbeed99c80342294
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2024-09-03 17:18:41 -0300

    ♻️ Change exercise 03 implementation

--- src/03.py
+++ src/03.py
@@ -8,99 +8,61 @@
 # b) Podem existir dias sem faturamento, como nos finais de semana e feriados.
 # Estes dias devem ser ignorados no cálculo da média;
 
-# DÚVIDA
-# Aparentemente não foi incluido um JSON ou XML para fonte de dados, então criei um
+# UPDATE
+# Os dados agora vem do arquivo data.json, que foi disponibilizado pela empresa
+
+# RESULTADO
+# Média do faturamento por mês. AVG: 20865.3702
+# Maior e menor valor de faturamento por mês. MAX: 373.7838 | MIN: 48924.2448
+# Dias em que o faturamento foi maior que a média. DAYS: [1, 2, 3, 6, 8, 9, 16, 20, 21, 27] | TOTAL: 10 dias
 
 from sys import stdout
-
-data = {
-    '2023-03-01': {'faturamento': 1000.0, 'feriado': False},
-    '2023-03-02': {'faturamento': 1200.0, 'feriado': False},
-    '2023-03-03': {'faturamento': 1500.0, 'feriado': False},
-    '2023-03-04': {'faturamento': 0.0, 'feriado': True},
-    '2023-03-05': {'faturamento': 1800.0, 'feriado': False},
-    '2023-03-06': {'faturamento': 2000.0, 'feriado': False},
-    '2023-03-07': {'faturamento': 0.0, 'feriado': True},
-    '2023-03-08': {'faturamento': 0.0, 'feriado': True},
-    '2023-03-09': {'faturamento': 2200.0, 'feriado': False},
-    '2023-03-10': {'faturamento': 2500.0, 'feriado': False},
-    '2023-04-01': {'faturamento': 1200.0, 'feriado': False},
-    '2023-04-02': {'faturamento': 1500.0, 'feriado': False},
-    '2023-04-03': {'faturamento': 1800.0, 'feriado': False},
-    '2023-04-04': {'faturamento': 0.0, 'feriado': True},
-    '2023-04-05': {'faturamento': 2000.0, 'feriado': False},
-    '2023-04-06': {'faturamento': 2200.0, 'feriado': False},
-    '2023-04-07': {'faturamento': 0.0, 'feriado': True},
-    '2023-04-08': {'faturamento': 0.0, 'feriado': True},
-    '2023-04-09': {'faturamento': 2500.0, 'feriado': False},
-    '2023-04-10': {'faturamento': 2800.0, 'feriado': False},
-    '2023-04-11': {'faturamento': 3000.0, 'feriado': False},
-    '2023-04-12': {'faturamento': 0.0, 'feriado': True},
-    '2023-04-13': {'faturamento': 3200.0, 'feriado': False},
-    '2023-04-14': {'faturamento': 3500.0, 'feriado': False},
-    '2023-04-15': {'faturamento': 0.0, 'feriado': True},
-}
-
-def remove_holidays(data):
-    return {date: values for date, values in data.items() if not values['feriado']}
-
-def calc_avg(data):
-    avg_billing = {}
-
-    for date, values in data.items():
-        month = date.split('-')[1]
-
-        if month not in avg_billing:
-            avg_billing[month] = []
-
-        avg_billing[month].append(values['faturamento'])
-
-    for month, faturamentos in avg_billing.items():
-        avg = round(sum(faturamentos) / len(faturamentos), 4)
-        avg_billing[month] = avg
-
-    return avg_billing
+import functools
+import json
+from typing import List
 
 
-def calc_min_max(data):
-    min_max = {}
-
-    for date, values in data.items():
-        month = date.split('-')[1]
-
-        if month not in min_max:
-            min_max[month] = {'min': float('inf'), 'max': float('-inf')}
-
-        if values['faturamento'] < min_max[month]['min']:
-            min_max[month]['min'] = values['faturamento']
-        
-        if values['faturamento'] > min_max[month]['max']:
-            min_max[month]['max'] = values['faturamento']
-
-    return min_max
+with open('./data/data.json', 'r') as f:
+    data = json.load(f)
 
 
-def days_above_average(data):
-    days = {}
-    avg_billing = calc_avg(data)
-
-    for date, values in data.items():
-        month = date.split('-')[1]
-
-        if month not in days:
-            days[month] = 0
-
-        if values['faturamento'] > avg_billing[month]:
-            days[month] += 1
-
-    return days
+# Remove os dias em que o faturamento foi diferente de 0, ou seja, dias úteis
+def transform_data(data) -> dict:
+    return {values['dia']: values['valor'] for values in data if values['valor'] != 0}
 
 
-data = remove_holidays(data)
+# Solução em usar SUM, mas usando um REDUCE
+# Arredondamento com precisão de 4 casas decimais
+def calc_avg(data) -> float:
+    sum_values = functools.reduce(lambda x, y: x + y, data.values())
+    return round(sum_values / len(data), 4)
+
+
+# Solução sem usar MIN e MAX
+def calc_min_max(data) -> tuple[float, float]:
+    min_value = float('inf')
+    max_value = float('-inf')
+    
+    for value in data.values():
+        if value < min_value:
+            min_value = value
+        if value > max_value:
+            max_value = value
+    
+    return min_value, max_value
+
+
+# Solução usando For Comprehension
+def days_above_avg(data) -> List[int]:
+    avg = calc_avg(data)
+    return [day for day, value in data.items() if value > avg]
+
+
+data = transform_data(data)
 avg_billing = calc_avg(data)
-min_max = calc_min_max(data)
-days = days_above_average(data)
+min, max = calc_min_max(data)
+days = days_above_avg(data)
 
-stdout.write(f'Média do faturamento por mês: {avg_billing}\n')
-stdout.write(f'Maior e menor valor de faturamento por mês: {avg_billing}\n')
-stdout.write(f'Dias em que o faturamento foi maior que a média: {days}\n')
+stdout.write(f'Média do faturamento por mês. AVG: {avg_billing}\n')
+stdout.write(f'Maior e menor valor de faturamento por mês. MAX: {min} | MIN: {max}\n')
+stdout.write(f'Dias em que o faturamento foi maior que a média. DAYS: {days} | TOTAL: {len(days)} dias\n')
